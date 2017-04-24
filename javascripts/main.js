b9463d5a3347f437b952092013f492b81fbe666d
Isabel Ramos <iramos1226@gmail.com>
2017-04-23 20:12:00 -0500

    users and winner print to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,59 @@
 $(document).ready(function() {
 
+
 	firstUsernameArray = [];
 	secondUsernameArray = [];
 
-	console.log(secondUsernameArray);
+	const writeToDOM = () => {
+        let treehouseString = "";
+
+
+        for (let i=0; i < firstUsernameArray.length; i++) {
+	        treehouseString += `<div class="row col-sm-3">`;
+	        treehouseString += `<div class="thumbnail">`;
+	        treehouseString += `<img src="${firstUsernameArray[i].gravatar_url}"`;
+	        treehouseString += `<div class="caption">`;           
+	        treehouseString += `<h3>${firstUsernameArray[i].name}</h3>`;
+		    treehouseString += `<p>Total Points: ${firstUsernameArray[i].points.total}</p>`;
+	      	treehouseString += `</div></div></div>`;
+	    }
+
+	    for (let i=0; i < secondUsernameArray.length; i++) {
+	      	treehouseString += `<div class="row col-sm-3">`;
+	        treehouseString += `<div class="thumbnail">`;
+	        treehouseString += `<img src="${secondUsernameArray[i].gravatar_url}"`;
+	        treehouseString += `<div class="caption">`;           
+	        treehouseString += `<h3>${secondUsernameArray[i].name}</h3>`;
+		    treehouseString += `<p>Total Points: ${secondUsernameArray[i].points.total}</p>`;
+	      	treehouseString += `</div></div></div>`;
+    	}
+
+	    for (let i=0; i < firstUsernameArray.length; i++) {
+	    	for (let j=0; j < secondUsernameArray.length; j++) {
+
+		    	let user1Points = firstUsernameArray[i].points;
+				let user2Points = secondUsernameArray[i].points;
+
+				if (user1Points.total > user2Points.total) {
+					treehouseString += `<div class="winner"><h2>${firstUsernameArray[i].name} wins!</h2></div>`;
+				} else {
+					treehouseString += `<div class="winner"><h2>${secondUsernameArray[i].name} wins!</h2></div>`;
+				}
+	    	}
+	    }
+
+        $("#container").append(treehouseString);
+    };
+
+
+	const buildURL = (userInputString) => {
+		return "https://teamtreehouse.com/" + userInputString + ".json";
+	};
 
 
 	const loadFirstUsername = () => {
 		return new Promise((resolve, reject) => {
+			// $.ajax(buildURL($("#userInput1").val()))
 			$.ajax("https://teamtreehouse.com/isabelramos.json")
 			.done((data) => resolve(data))
 			.fail((error) => reject(error));
@@ -16,6 +62,7 @@ $(document).ready(function() {
 
 	const loadSecondUsername = () => {
 		return new Promise((resolve, reject) => {
+			// $.ajax(buildURL($("#userInput2").val()))
 			$.ajax("https://teamtreehouse.com/krissycaron.json")
 			.done((data2) => resolve(data2))
 			.fail((error) => reject(error));
@@ -23,6 +70,7 @@ $(document).ready(function() {
 	};
 
 
+	$("#battle-button").click((event) => {
 		Promise.all([loadFirstUsername(), loadSecondUsername()])
 		.then(function(result){
 			result.forEach(function(xhrCall, index) {
@@ -33,15 +81,13 @@ $(document).ready(function() {
 				}
 			});
 			
-			// writeToDOM();
+			writeToDOM();
 		})
-		.catch(function(userError){
-			console.log(userError);
+		.catch(function(usernameError){
+			console.log(usernameError);
 		});
 
-
-
-
+	});
 
 
 
